505d3592d5eb9b40f545d9cb566a20df67bb5146
Varun Agrawal <varagrawal@gmail.com>
2017-12-09 19:49:43 -0500

    day3 completed - and it works really fast

--- 2017/day3.py
+++ 2017/day3.py
@@ -1,5 +1,7 @@
-
 """Solution for day 3"""
+
+import numpy as np
+
 
 def steps(x):
     i = 1
@@ -62,5 +64,87 @@ def part1():
     print(steps(312051))
 
 
-def part2():
-    pass
+class SpiralWalker:
+    def __init__(self):
+        self.direction = 'r'
+        self.directions = ['r', 'u', 'l', 'd']
+        self.spiral = np.zeros((558, 558)) * -1
+        self.position = [0, 0]
+
+    def toggle_direction(self):
+        idx = self.directions.index(self.direction)
+        self.direction = self.directions[(idx+1) % len(self.directions)]
+
+    def step(self):
+        if self.direction == 'r':
+            self.position[1] += 1
+
+        elif self.direction == 'l':
+            self.position[1] -= 1
+        elif self.direction == 'u':
+            self.position[0] += 1
+        elif self.direction == 'd':
+            self.position[0] -= 1
+        
+    def print(self):
+        print(self.spiral)
+
+
+def get_value(spiral, x, y):
+    val = 0
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            val += spiral[y+j, x+i]
+    return val
+
+
+def part2(inp):
+    l = int(np.sqrt(inp)) + 2
+    spiral = np.zeros((l, l))
+    x, y = l // 2, l // 2
+    spiral[y, x] = 1
+
+    directions = ['>', '^', '<', 'v']
+
+    direction = directions[0]
+    idx = 0
+    turns = 0
+    steps = 1
+    counter = 1
+    limit = 1
+
+    while True:
+        if direction == '>':
+            x += 1
+        elif direction == '<':
+            x -= 1
+        elif direction == '^':
+            y -= 1
+        elif direction == 'v':
+            y += 1
+        
+        if x >= l or y >= l:
+            break
+
+        # take a step
+        steps += 1
+        counter += 1
+        val = get_value(spiral, x, y)
+        spiral[y, x] = val
+        
+        if val > inp:
+            print(val)
+            return
+
+        # turn since we've reached the limit for the current direction
+        if steps >= limit:
+            idx = (idx + 1) % len(directions)
+            direction = directions[idx]
+            if direction in ['>', '<']: # move in left or right direction
+                limit += 1 
+            steps = 0
+
+    # print(spiral)
+
+# part2(9)
+part2(312051)
